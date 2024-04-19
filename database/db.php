2bbea6b521b474f899b47a6290d65bdaddca85ff
SimoneCerri <simone.cerri1994@gmail.com>
2024-04-19 16:30:40 +0200

    modify layout of page, problem to take Movie->duration or TVShow->seasons

--- database/db.php
+++ database/db.php
@@ -7,12 +7,12 @@ require_once __DIR__ . "/../Models/TVShow.php"; //import class
 $productions =
     [
         new Production("Avatar", "Avatarese", 7, new Genre("Strange", "Is a fantasy about a big blue people, not Puffi")),
+        new Production("Back to the Future", "English", 10, new Genre("Future", "Is not about where, but WHEN")),
+        new TVShow("Peaky Blinders", "British", 9, new Genre("Crime", "FOKKING PEAKY BLINDEH"), 6),
+        new TVShow("The Big Bang Theory", "English", 10, new Genre("Comedy", "Our whole universe was in a hot dense state, Then nearly fourteen billion years ago expansion started. Wait..."), 12),
         new Production("StarWars", "English", 10, new Genre("Fantasy", "One of the best saga in history")),
         new Production("Batman", "English", 6, new Genre("Fantasy", "I'm Batman...")),
-        new Production("Back to the Future", "English", 10, new Genre("Future", "Is not about where, but WHEN")),
         new Production("The Martian", "Martian", 8, new Genre("Future", "")),
         new Production("Kill Bill vol.1", "English", 10, new Genre("Historical", "Overkill peoples")),
         new Movie("The Departed", "English", 8, new Genre("Action", "Kill, resurrect, not death, kill again, repeat"), 7000000, 150),
-        new TVShow("The Big Bang Theory", "English", 10, new Genre("Comedy", "Our whole universe was in a hot dense state, Then nearly fourteen billion years ago expansion started. Wait..."), 12),
-        new TVShow("Peaky Blinders", "British", 9, new Genre("Crime", "FOKKING PEAKY BLINDEH"), 6),
     ];
--- index.php
+++ index.php
@@ -3,8 +3,6 @@ require_once __DIR__ . "/Models/Production.php"; //import class
 require_once __DIR__ . "/Models/Genre.php"; //import class
 require_once __DIR__ . "/Models/Movie.php"; //import class
 require_once __DIR__ . "/Models/TVShow.php"; //import class
-
-
 require __DIR__ . "/database/db.php";
 ?>
 
@@ -19,14 +17,6 @@ require __DIR__ . "/database/db.php";
     <!-- #region link to Style -->
     <link href='./assets/css/style.css' rel='stylesheet'>
     <!-- #endregion link to Style -->
-    <!-- #region link to FontAwesome (icons) -->
-    <link rel='stylesheet' href='https: //cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.    css' integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIft+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==' crossorigin='anonymous' referrerpolicy='no-referrer' />
-    <!-- #endregion link to FontAwesome (icons) -->
-    <!-- #region links to GoogleFonts (Nanum Gothic Coding) -->
-    <link rel='preconnect' href='https: //fonts.googleapis.com'>
-    <link rel='preconnect' href='https: //fonts.gstatic.com' crossorigin>
-    <link href='https: //fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap' rel='stylesheet'>
-    <!-- #endregion links to GoogleFonts (Nanum Gothic Coding) -->
     <!-- #region link to Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- #endregion link to Bootstrap -->
@@ -34,72 +24,161 @@ require __DIR__ . "/database/db.php";
 </head>
 
 <body class='debug'>
-
-    <div id='app'>
-
-        <div class="container">
-            <div class="container py-5">
-                <div class="row g-3">
-                    <?php foreach ($productions as $production) : ?>
-                        <div class="col-4">
-                            <div class="card text-center h-100">
-                                <div class="card-title">
-                                    <strong>
-                                        Title: <?php echo $production->title ?>
-                                    </strong>
-                                    <hr>
-                                </div>
-                                <div class="card-body">
-                                    <div>
-                                        <span>
-                                            Language: <?php echo $production->language ?>
-                                        </span>
+    <style>
+        <?php include './assets/css/style.css'; ?>
+    </style>
+    <header>
+        <div class="logo">
+            <img src="./assets/img/logo.svg" alt="">
+        </div>
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" id="navId" role="tablist">
+            <li class="nav-item">
+                <a href="#tab1Id" class="nav-link" data-bs-toggle="tab" aria-current="page">About us</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a href="#tab5Id" class="nav-link" data-bs-toggle="tab">Another link</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a href="#" class="nav-link" data-bs-toggle="tab">Something</a>
+            </li>
+        </ul>
+    </header>
+    <main>
+        <div id='app'>
+            <div class="container">
+                <div class="container py-5">
+                    <div class="row g-3">
+                        <?php foreach ($productions as $production) : ?>
+                            <div class="col-4">
+                                <div class="card text-center h-100">
+                                    <div class="card-title">
+                                        <strong>
+                                            Title: <?php echo $production->title ?>
+                                        </strong>
                                     </div>
-                                    <div>
-                                        <span>
-                                            Vote: <?php echo $production->vote ?> /10
-                                        </span>
+                                    <div class="card-body">
+                                        <div>
+                                            <span>
+                                                Language: <?php echo $production->language ?>
+                                            </span>
+                                        </div>
+                                        <div>
+                                            <span>
+                                                Vote: <?php echo $production->vote ?> /10
+                                            </span>
+                                        </div>
+                                        <div>
+                                            <!-- <span>
+                                                Profit: <?php echo $production->movie?->profit ?>
+                                            </span> -->
+                                        </div>
+                                        <div>
+                                            <!-- <span>
+                                                Duration: <?php echo $production->movie?->duration ?>
+                                            </span> -->
+                                        </div>
+                                        <div>
+                                            <!-- <span>
+                                                Seasons: <?php echo $production->tvshow?->season ?>
+                                            </span> -->
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="card-footer">
-                                    <div>
-                                        <span>
-                                            Type: <?php echo $production->genre?->name ?>
-                                            <!-- $production->genre?->name -->
-                                            <!-- the "?" is equal to do an if to controll if genre exist -->
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <span>
-                                            <?php echo $production->genre?->description ?>
-                                        </span>
+                                    <div class="card-footer">
+                                        <div>
+                                            <span>
+                                                Type: <?php echo $production->genre?->name ?>
+                                                <!-- $production->genre?->name -->
+                                                <!-- the "?" is equal to do an if to controll if genre exist -->
+                                            </span>
+                                        </div>
+                                        <div>
+                                            <span>
+                                                <?php echo $production->genre?->description ?>
+                                            </span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
             </div>
-
         </div>
+    </main>
 
-        <style>
-            body {
-                background-color: #333;
-                color: white;
-            }
-        </style>
-    </div>
+    <footer>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-6 left">
+                    <ul>
+                        <li>
+                            <strong>
+                                <h4>
+                                    Quality
+                                </h4>
+                            </strong>
+                        </li>
+                        <li>
+                            <a href="">
+                                Number_1
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Number_2
+                            </a>
+                        </li>
+                        <li><a href="">
+                                Number_3
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Number_4
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-6 right">
+                    <ul>
+                        <li>
+                            <strong>
+                                <h4>
+                                    Quantity
+                                </h4>
+                            </strong>
+                        </li>
+                        <li>
+                            <a href="">
+                                1
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                2
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                3
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                4
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <!-- #region Dev-only cdns, disable in production -->
-    <!-- #region link to Vue-script -->
-    <script src='https: //unpkg.com/vue@3/dist/vue.global.js'></script>
-    <!-- #endregion link to Vue-script -->
     <!-- #region link to Bootstrap-script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- #endregion link to Bootstrap-script -->
-    <!-- #region link to Script -->
-    <script src='./assets/js/main.js'></script>
-    <!-- #endregion link to Script -->
     <!-- #endregion Dev-only cdns, disable in production -->
 
 </body>
